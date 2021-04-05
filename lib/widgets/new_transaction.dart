26a89dbf2990cc625300c15318c699b3f41088a3
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-05 20:33:49 +0100

    adding user transactions to the list

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
 
-
 class NewTransaction extends StatelessWidget {
-
+  final Function addTx;
   final TextEditingController titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: "Title"),
-                    //we can omit the type String cuz Dart will infer it
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "Title"),
+              //we can omit the type String cuz Dart will infer it
 
-                    //instead of registering the input manually on each key stroke
-                    //Flutter provides us with an alternative that allows us to listne to each key stroke and get the finished value once we're done
-                    controller: titleController,
+              //instead of registering the input manually on each key stroke
+              //Flutter provides us with an alternative that allows us to listne to each key stroke and get the finished value once we're done
+              controller: titleController,
 
-                    //onChanged: (String val) {
-                    //  titleInput = val;
-                    //},
-                  ),
-                  TextField(
-                    controller: amountController,
-                    decoration: InputDecoration(labelText: "Amount"),
-                    //onChanged: (val) => amountInput = val,
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print(titleController.text);
-
-                      //TODO : we need to trigger the _addTransaction method 
-
-                      //print(titleInput);
-                      //print(amountInput);
-                    },
-                    child: Text("Add Transaction"),
-                    textColor: Colors.purple,
-                  ),
-                ],
-              ),
+              //onChanged: (String val) {
+              //  titleInput = val;
+              //},
             ),
-          );
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(labelText: "Amount"),
+              //onChanged: (val) => amountInput = val,
+            ),
+            FlatButton(
+              onPressed: () {
+                print(titleController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+
+                //TODO : we need to trigger the _addTransaction method
+
+                //print(titleInput);
+                //print(amountInput);
+              },
+              child: Text("Add Transaction"),
+              textColor: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
