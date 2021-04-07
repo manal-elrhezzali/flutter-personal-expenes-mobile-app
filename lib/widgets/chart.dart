20587bd1f9986cd0d62e19be895ddc887264731c
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-07 23:20:58 +0100

    creating Bars for the Chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -4,27 +4,31 @@ import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
-  
+
   Chart(this.recentTransactions);
-  
+
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
       var totalSum = 0.0;
-      for(var i = 0; i < recentTransactions.length; i++){
-        if(recentTransactions[i].date.day == weekDay.day && 
-        recentTransactions[i].date.month == weekDay.month && 
-        recentTransactions[i].date.year == weekDay.year){
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
       print(DateFormat.E().format(weekDay));
       print(totalSum);
 
-      return {"day": DateFormat.E().format(weekDay), "amount": totalSum};
+      return {
+        "day": DateFormat.E().format(weekDay).substring(0, 1),
+        "amount": totalSum,
+      };
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +37,13 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [
-
-        ],
+        children:
+            //map takes a function that executes on each
+            //groupedTransactionValue slist items and returns a widget
+            groupedTransactionValues.map((data) {
+          // return Text(data["day"] + " : " + data["amount"].toString(),);
+          return Text("${data["day"]}: ${data["amount"]}");
+        }).toList(),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentageOfTotal);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        //the Chart Bar
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  //from RGBO allows us to mix colors
+                  //from red,green and blue
+                  //with a defined opacity
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              //this is overlapped on top of the Stack's child contained
+              FractionallySizedBox(
+                heightFactor: spendingPercentageOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
