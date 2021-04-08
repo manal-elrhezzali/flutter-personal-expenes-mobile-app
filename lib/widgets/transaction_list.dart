a87f2210e349e399e0edf71313ef3f29f8a4b89e
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-08 13:13:19 +0100

    using a ListTile widget in the transactions list item

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -30,59 +30,80 @@ class TransactionList extends StatelessWidget {
               ],
             )
           : ListView.builder(
+              itemCount: transactions.length,
               itemBuilder: (context, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "\$${transactions[index].amount.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: ListTile(
+                    //CircleAvatar() is a rounded widget that can hold a content
+                    // it is often used to hold images
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text("\$${transactions[index].amount}")),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                            // style: TextStyle(
-                            // fontSize: 16,
-                            // fontWeight: FontWeight.bold,
-                            // ),
-                          ),
-                          Text(
-                            //DateFormat().format(tx.date),
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.title),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         margin:
+                //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           "\$${transactions[index].amount.toStringAsFixed(2)}",
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Text(
+                //             transactions[index].title,
+                //             style: Theme.of(context).textTheme.title,
+                //             // style: TextStyle(
+                //             // fontSize: 16,
+                //             // fontWeight: FontWeight.bold,
+                //             // ),
+                //           ),
+                //           Text(
+                //             //DateFormat().format(tx.date),
+                //             DateFormat.yMMMd().format(transactions[index].date),
+                //             style: TextStyle(
+                //               fontSize: 12,
+                //               color: Colors.grey,
+                //             ),
+                //           ),
+                //         ],
+                //       ),
+                //     ],
+                //   ),
+                // );
               },
               //number of elements in the list so that Flutter get to know
               //that the anonymous function passed to the itemBuilder
               //needs to be executed as many times as
               //the length of the list of transactions
-              itemCount: transactions.length,
             ),
     );
   }
