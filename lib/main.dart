e10b5e605ed7b046f2ef6fdc094945c3885445e0
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-08 19:00:43 +0100

    Rendering Alternaative Landscape content

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ import './widgets/new_transaction.dart';
 import './models/transaction.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
   runApp(MyApp());
 }
 
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expenses', 
+      title: 'Personal Expenses',
       theme: ThemeData(
         // errorColor: Colors.red, //the errorColor is red by default
         primarySwatch: Colors.purple,
@@ -59,6 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
   final titleController = TextEditingController();
 
   final amountController = TextEditingController();
+
+  bool _showChart = false;
 
   final List<Transaction> _userTransactions = [
     // Transaction(
@@ -150,15 +152,31 @@ class _MyHomePageState extends State<MyHomePage> {
           //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Show Chart"),
+                Switch(
+                  value: _showChart,
+                  onChanged: (val) {
+                    setState(() {
+                      _showChart = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+            _showChart ? Container(
               height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height - MediaQuery.of(context).padding.top) *
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
                   0.3,
               child: Chart(_recentTransactions),
-            ),
+            ) :
             Container(
               height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height  - MediaQuery.of(context).padding.top) *
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
                   0.7,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
